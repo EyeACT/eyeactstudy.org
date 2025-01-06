ae912a8a1766e789a63d57ebbdbef89861147a2c
slugb0t <wheresdorian@gmail.com>
2025-01-06 10:00:21 -0800

    refactor: :recycle: modify event content to appear from left to right within card

--- src/components/events/EventsLayout.tsx
+++ src/components/events/EventsLayout.tsx
@@ -9,30 +9,28 @@ const EventsLayout: React.FC<EventsLayoutProps> = ({ eventList }) => {
     <>
       <hr className='mx-6 my-1 border-dashed border-slate-300' />
 
-      <div className='flex flex-col gap-6 px-8'>
+      <div className='flex flex-wrap gap-6 px-8'>
         {eventList.map((event) => (
           <div
             key={event.slug}
-            className='group relative flex flex-col rounded-lg border border-gray-200 bg-white shadow-lg transition-shadow duration-300 hover:shadow-2xl'
+            className='group relative flex flex-row rounded-lg border border-gray-200 bg-white shadow-lg transition-shadow duration-300 hover:shadow-2xl'
           >
-            <Link href={`/events/${event.slug}`} passHref>
-              <div className='relative h-56 overflow-hidden rounded-t-lg'>
-                <Image
-                  src={event.frontMatter.heroImage}
-                  alt={event.frontMatter.title}
-                  fill
-                  className='h-full w-full object-cover object-center transition-transform duration-300 group-hover:scale-105'
-                  style={{ aspectRatio: '16 / 9' }} // Dynamically adjust aspect ratio
-                />
-                {dayjs(event.frontMatter.startDateTime).isAfter(dayjs()) && (
-                  <span className='absolute left-4 top-4 rounded bg-blue-500 px-2 py-1 text-xs font-semibold text-white'>
-                    Upcoming
-                  </span>
-                )}
-              </div>
-            </Link>
-
-            <div className='flex flex-col p-5'>
+            <div className='relative w-full max-w-[40%] overflow-hidden rounded-l-lg'>
+              <Image
+                src={event.frontMatter.heroImage}
+                alt={event.frontMatter.title}
+                layout='responsive'
+                width={16} // Aspect ratio width
+                height={9} // Aspect ratio height
+                className='object-centern object-cover transition-transform duration-300 hover:scale-105'
+              />
+              {dayjs(event.frontMatter.startDateTime).isAfter(dayjs()) && (
+                <span className='absolute left-4 top-4 rounded bg-blue-500 px-2 py-1 text-xs font-semibold text-white'>
+                  Upcoming
+                </span>
+              )}
+            </div>
+            <div className='flex flex-1 flex-col p-5'>
               <Link href={`/events/${event.slug}`} passHref>
                 <h3 className='text-lg font-bold text-gray-800 transition duration-300 group-hover:text-blue-500'>
                   {event.frontMatter.title}
@@ -41,7 +39,6 @@ const EventsLayout: React.FC<EventsLayoutProps> = ({ eventList }) => {
               <p className='mt-2 text-sm text-gray-600'>
                 {event.frontMatter.subtitle}
               </p>
-
               <div className='mt-4 border-t pt-4 text-sm text-gray-500'>
                 <EventDates
                   startDateTime={event.frontMatter.startDateTime}
